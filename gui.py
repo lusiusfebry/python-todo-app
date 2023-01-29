4061a2b0e3caaea3db3da3d61b71783376e50c21
febrianus.maro <lusiusfebry@gmail.com>
2023-01-29 12:58:47 +0800

    add gui app with simplepy

---
+++ gui.py
@@ -0,0 +1,10 @@
+import function
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box  = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My To-Do app",layout=[[label],[input_box],[add_button]])
+window.read()
+window.close()
